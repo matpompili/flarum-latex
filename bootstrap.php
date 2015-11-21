fde1ce23c34342fd83a44d01ce44a2464f05ea9f
Matteo Pompili <matpompili@gmail.com>
2015-11-21 12:40:54 +0100

    Now the extensions can be displayed in admin panel. Also it adds a message to every new post

--- bootstrap.php
+++ bootstrap.php
@@ -10,6 +10,12 @@
  */
 
 
-return function () {
-  echo 'Hello, world!';
-}
+ use Flarum\Event\PostWillBeSaved;
+ use Illuminate\Contracts\Events\Dispatcher;
+
+ return function (Dispatcher $events) {
+     $events->listen(PostWillBeSaved::class, function (PostWillBeSaved $event) {
+         // do stuff before a post is saved
+         $event->post->content .= ' ##MathJax extension is working. Waiting to be operative';
+     });
+ };
