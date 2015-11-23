5c6eed9a64ac089ec30cbfa2f68a8f11c4e44264
Matteo Pompili <matpompili@gmail.com>
2015-11-23 23:19:02 +0100

    Now on PostWillBeSaved it searches for LaTeX delimiters and prevent markup operation on them

--- bootstrap.php
+++ bootstrap.php
@@ -12,6 +12,8 @@
 
 use Illuminate\Contracts\Events\Dispatcher;
 use Flarum\Event\ConfigureClientView;
+use Flarum\Event\PostWillBeSaved;
+
 
 return function (Dispatcher $events) {
   $events->listen(ConfigureClientView::class, function (ConfigureClientView $event) {
@@ -20,7 +22,19 @@ return function (Dispatcher $events) {
       $event->view->addHeadString('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.5.1/katex.min.css">');
       $event->view->addHeadString('<script src="https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.5.1/katex.min.js"></script>');
       $event->view->addHeadString('<script src="https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.5.1/contrib/auto-render.min.js"></script>');
+      //$event->view->addHeadString('<script type="text/x-mathjax-config">MathJax.Hub.Config({tex2jax: {inlineMath: [ ["$","$"], ["\\(","\\)"] ],displayMath: [ ["$$","$$"], ["\\[","\\]"] ],processEscapes: true,}})</script>');
+      //$event->view->addHeadString('<script type="text/javascript" src="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>');
       $event->addBootstrapper('matpompili/flarum-latex/main');
     }
   });
+  $events->listen(PostWillBeSaved::class, function (PostWillBeSaved $event) {
+      $text = $event->post->content;
+      $event->post->content =  preg_replace('/(?<!\\\\)(?: ((?<!\\$)(?<!\\`)\\${1,2}(?!\\`)(?!\\$)))(.*(?R)?.*)(?<!\\\\)(?: ((?<!\\$)(?<!\\`)\\1(?!\\`)(?!\\$)))/mxU', '`\\1\\2\\3`', $text);
+  });
 };
+
+/*
+Questa è l'espressione della forza:
+$$\vec{F} = -G \frac{m_1 m_2}{|\vec{r}^3|}\vec{r}$$
+Il segno $-$ davanti alla costante $G$ indica che la forza è attrattiva, ovvero è di verso opposto al vettore $\vec{r}$.
+*/
